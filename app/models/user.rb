60f8af4deb05bc935d5214445df2ca30f5d9cf0b
Kailey Lee McIntosh <klee.mcintosh@gmail.com>
2019-12-15 21:01:59 -0500

    login and signup validation and errors added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :libraries
-  validates :username, uniqueness: { case_sensitive: false }
+  validates :username, uniqueness: { case_sensitive: false }, presence: true
+  validates :password, presence: true
+  validates :email, presence: true
 
 end
